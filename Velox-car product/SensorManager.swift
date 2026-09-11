95adbddd4ffd7815e08a2b69fba865cd412c3119
stepanchikk <122825623+stepanchikk@users.noreply.github.com>
2026-09-11 22:21:48 +0300

    Implement accelerometer data collection in SensorManager
    Added functionality to start and stop accelerometer updates, including properties to store acceleration data.

--- Velox-car product/SensorManager.swift
+++ Velox-car product/SensorManager.swift
@@ -1,8 +1,38 @@
-//
-//  SensorManager.swift
-//  Velox-car product
-//
-//  Created by Stepan on 10.09.2026.
-//
-
 import Foundation
+import CoreMotion
+import Combine
+
+class SensorManager: ObservableObject {
+    // Головний об'єкт Apple для роботи з датчиками руху
+    private var motionManager = CMMotionManager()
+    
+    // Властивості @Published автоматично оновлюватимуть інтерфейс при зміні значень
+    @Published var x: Double = 0.0
+    @Published var y: Double = 0.0
+    @Published var z: Double = 0.0
+    @Published var isRecording: Bool = false
+    
+    func startSensors() {
+        // Перевірка, чи датчик взагалі доступний
+        if motionManager.isAccelerometerAvailable {
+            //Частота оновлень: 0.1 секунди
+            motionManager.accelerometerUpdateInterval = 0.1
+            
+            // Запуск збору даних в основному потоці
+            motionManager.startAccelerometerUpdates(to: .main) { [weak self] data, error in
+                guard let data = data, error == nil else { return }
+                
+                // Запис отриманих сирих даних у змінні
+                self?.x = data.acceleration.x
+                self?.y = data.acceleration.y
+                self?.z = data.acceleration.z
+            }
+            isRecording = true
+        }
+    }
+    
+    func stopSensors() {
+        motionManager.stopAccelerometerUpdates()
+        isRecording = false
+    }
+}
